0f3c5c464b7b03ef363c5757661244d1477d5f41
csunking <csunking@gmail.com>
2023-01-28 18:26:46 +0800

    feat, add debugger

--- src/test_fisrt.py
+++ src/test_fisrt.py
@@ -8,9 +8,10 @@
 # date 2023-01-27 23:08
 #
 
-import bpy
 from random import randint
-import numpy as np
+
+import bpy
+import pydevd_pycharm
 
 """
 
@@ -22,12 +23,19 @@ blender api: https://docs.blender.org/api/current/info_quickstart.html
 F:\DS\_ws\blender\3.4\python\bin
 bpy的安装：F:\DS\_ws\blender\3.4\python\bin>.\python.exe -m pip install fake-bpy-module-3.4
 
-调试：https://blog.csdn.net/sam_shan/article/details/120995582
+调试
+https://blog.csdn.net/weixin_43775400/article/details/124777871
+https://zhuanlan.zhihu.com/p/595184674
+F:\DS\_ws\blender\3.4\python\bin>.\python.exe -m pip install pydevd-pycharm~=223.7571.182
+在 D:\Pros\IDE\Cache\.IntelliJIdea2019.2\config\plugins\python\debugger-eggs-output 中找到 pydevd-pycharm.egg
+
+
 
 Examples
 https://blog.csdn.net/qq_43331089/article/details/123397323
 """
 
+pydevd_pycharm.settrace('localhost', port=1090, stdoutToServer=True, stderrToServer=True)
 
 # 随机创建100个cube，x/y/z范围限制（-30，30）
 number = 100
